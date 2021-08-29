a653284be215ab8c74a2f387d9f80ad90e8037ac
ABdevs29 <abhishek.elmoro@gmail.com>
2021-08-29 18:58:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Sidebar from "./Sidebar";
+import Topbar from "./Topbar";
+import Dashboard from "./Dashboard";
+import User from "./User";
+import Product from "./Product";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div id="wrapper">
+        <Sidebar></Sidebar>
+        <div id="content-wrapper" class="d-flex flex-column">
+          <div id="content">
+            <Topbar></Topbar>
+            <div class="container-fluid">
+              <Switch>
+                <Route path="/" component={Dashboard} exact={true}></Route>
+                <Route path="/user" component={User} exact={true}></Route>
+                <Route path="/product" component={Product} exact={true}></Route>
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
